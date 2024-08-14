f27697b0de6ec6db2956866b3fef3b8f0824c5d7
berakpaijo <iyanmaumakan56@gmail.com>
2024-08-14 12:44:33 +0700

    VERY IMPORTANT UPDATE, 100 PETABYTES IN TOTAL. Patch 0.0.1 Version 1.0.1.1

--- themes/theme.h
+++ themes/theme.h
@@ -9,12 +9,12 @@ Macchiato macchiato;
 Mocha mocha;
 
 // Color palette
-extern const Color GridC = mocha.MochaSurface2;
-extern const Color LBlockC = mocha.MochaPeach;
-extern const Color JBlockC = mocha.MochaBlue;
-extern const Color IBlockC = mocha.MochaSky;
-extern const Color OBlockC = mocha.MochaYellow;
-extern const Color SBlockC = mocha.MochaGreen;
-extern const Color TBlockC = mocha.MochaMauve;
-extern const Color ZBlockC = mocha.MochaRed;
-extern const Color BaseC = mocha.MochaBackground;
+inline const Color GridC = mocha.MochaSurface2;
+inline const Color LBlockC = mocha.MochaPeach;
+inline const Color JBlockC = mocha.MochaBlue;
+inline const Color IBlockC = mocha.MochaSky;
+inline const Color OBlockC = mocha.MochaYellow;
+inline const Color SBlockC = mocha.MochaGreen;
+inline const Color TBlockC = mocha.MochaMauve;
+inline const Color ZBlockC = mocha.MochaRed;
+inline const Color BaseC = mocha.MochaBackground;
